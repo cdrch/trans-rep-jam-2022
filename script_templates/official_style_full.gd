299f8e9a5bf2e288f869da12f5f48e9bf0b10a87
Carter David Henning <carterdavidhenning@gmail.com>
2022-06-27 12:31:14 -0400

    Add script template
    Template is based on the official Godot style guide.

---
+++ script_templates/official_style_full.gd
@@ -0,0 +1,45 @@
+#class_name 
+extends %BASE%
+# Script description goes here
+
+# Signals
+
+
+# Member variables
+
+
+# onready variables
+
+
+# Constants
+
+
+# Enums
+
+
+# Core functions
+#func _init()%VOID_RETURN%:
+
+
+#func _ready()%VOID_RETURN%:
+
+
+#func _input(event)%VOID_RETURN%:
+
+
+#func _unhandled_input(event)%VOID_RETURN%:
+
+
+#func _process(delta%FLOAT_TYPE%)%VOID_RETURN%:
+
+
+#func _physics_process(delta%FLOAT_TYPE%)%VOID_RETURN%:
+
+
+# Getters/Setters
+
+
+# Public functions
+
+
+# Private functions
