a8048b96303b1d8cab1fea0bc4b640dac123fea5
Carter David Henning <carterdavidhenning@gmail.com>
2022-06-27 14:37:20 -0400

    Add player bullet firing point

---
+++ player_firing_point.gd
@@ -0,0 +1,46 @@
+#class_name 
+extends Node2D
+# Script description goes here
+
+# Signals
+
+
+# Member variables
+
+
+# onready variables
+
+
+# Constants
+
+
+# Enums
+
+
+# Core functions
+#func _init() -> void:
+
+
+#func _ready() -> void:
+
+
+func _input(event) -> void:
+	if event.is_action_pressed("fire"):
+		pass # TODO: shoot bullet
+
+#func _unhandled_input(event) -> void:
+
+
+#func _process(delta: float) -> void:
+
+
+#func _physics_process(delta: float) -> void:
+
+
+# Getters/Setters
+
+
+# Public functions
+
+
+# Private functions
