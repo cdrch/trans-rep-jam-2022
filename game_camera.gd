bb4efcf8c402a9324caae5619392e90416d64da7
Carter David Henning <carterdavidhenning@gmail.com>
2022-06-27 13:38:29 -0400

    Add camera controller

---
+++ game_camera.gd
@@ -0,0 +1,45 @@
+class_name GameCamera 
+extends Camera2D
+# Main camera controller for the game.
+
+# Signals
+
+
+# Member variables
+
+
+# onready variables
+
+
+# Constants
+
+
+# Enums
+
+
+# Core functions
+#func _init() -> void:
+
+
+#func _ready() -> void:
+
+
+#func _input(event) -> void:
+
+
+#func _unhandled_input(event) -> void:
+
+
+#func _process(delta: float) -> void:
+
+
+#func _physics_process(delta: float) -> void:
+
+
+# Getters/Setters
+
+
+# Public functions
+
+
+# Private functions
