95f45919218d5aeb91806d72a0c86b3d40dc9535
Carter David Henning <carterdavidhenning@gmail.com>
2022-06-27 13:35:40 -0400

    Add player ship controls and collision

---
+++ player_ship.gd
@@ -0,0 +1,71 @@
+#class_name 
+extends KinematicBody2D
+# Script description goes here
+
+# Signals
+
+
+# Member variables
+export(float) var speed: float
+var velocity: Vector2
+
+
+# onready variables
+
+
+# Constants
+
+
+# Enums
+
+
+# Core functions
+#func _init() -> void:
+
+
+#func _ready() -> void:
+
+
+#func _input(event) -> void:
+
+
+#func _unhandled_input(event) -> void:
+
+
+func _process(delta: float) -> void:
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if not collision.collider is Node:
+			velocity = velocity.slide(collision.normal)
+			return
+		else:
+			var collision_node: Node = collision.collider
+			var groups = collision_node.get_groups()
+			if groups.has("solid"):
+				velocity = velocity.slide(collision.normal)
+				return
+			elif groups.has("bullet") and not groups.has("player"):
+				# TODO: oh no boom
+				return
+
+
+# Getters/Setters
+
+
+# Public functions
+
+
+# Private functions
