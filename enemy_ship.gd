e7fc053bed83a6fe14498547accec85bec43d64f
Carter David Henning <carterdavidhenning@gmail.com>
2022-06-29 17:10:10 -0400

    Add enemy ship

---
+++ enemy_ship.gd
@@ -0,0 +1,45 @@
+class_name EnemyShip
+extends KinematicBody2D
+# The controller for enemy ships.
+
+# Signals
+
+
+# Member variables
+
+
+# onready variables
+
+
+# Constants
+
+
+# Enums
+
+
+# Core functions
+#func _init() -> void:
+
+
+#func _ready() -> void:
+
+
+#func _input(event) -> void:
+
+
+#func _unhandled_input(event) -> void:
+
+
+#func _process(delta: float) -> void:
+
+
+#func _physics_process(delta: float) -> void:
+
+
+# Getters/Setters
+
+
+# Public functions
+
+
+# Private functions
