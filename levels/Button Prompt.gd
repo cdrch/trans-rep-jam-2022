254a41bc5200c7c7726187f9834f968ed34e92ee
Carter David Henning <carterdavidhenning@gmail.com>
2022-08-18 16:06:50 -0400

    Add basic button prompt prefab

---
+++ levels/Button Prompt.gd
@@ -0,0 +1,10 @@
+extends Control
+
+export(String) var desired_input
+
+signal responded_to_prompt
+
+func _process(delta):
+	if Input.is_action_pressed(desired_input):
+		emit_signal("responded_to_prompt")
+		queue_free()
